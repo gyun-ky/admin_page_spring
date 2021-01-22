a8e37e0bd467b0d98b204ce1b4b0f2409e3c898c
kimkiyun <kiyoon9390@gmail.com>
2021-01-23 03:22:09 +0900

    db 설정 and entity

--- src/main/java/com/example/study/controller/PostController.java
+++ src/main/java/com/example/study/controller/PostController.java
@@ -13,5 +13,4 @@ public class PostController {
         return searchParam;
     }
 
-    @PutMapping()
 }
---
+++ src/main/java/com/example/study/model/entity/User.java
@@ -0,0 +1,29 @@
+package com.example.study.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updateAt;
+
+    private String updateBy;
+}
